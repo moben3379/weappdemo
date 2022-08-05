c0039f4d79fc21394112b760d539907fea365e2b
moben3379 <102411528+moben3379@users.noreply.github.com>
2022-08-05 09:45:44 +0800

    Update GetMenuController.java

--- src/main/java/cn/edu/guet/weappdemo/controller/GetMenuController.java
+++ src/main/java/cn/edu/guet/weappdemo/controller/GetMenuController.java
@@ -49,13 +49,13 @@ public class GetMenuController {
     public HttpResult searchMenu(@RequestBody String searchKey){
         System.out.println(searchKey);
         return HttpResult.ok(getMenuService.searchMenu(searchKey));
-
+    }
 
     @GetMapping(value = "/addMenuInformation")//新增菜品数据
     public void addMenuInformation(@RequestParam Integer Id, @RequestParam String Name, @RequestParam double Price, @RequestParam String Form, @RequestParam Integer Count, @RequestParam String Icon, @RequestParam String Detail, HttpServletRequest request){
         System.out.println("新增的菜品ID为："+Id+Name+Price+Form+Count+Icon+Detail);
         getMenuService.AddFoodMenu(Id, Name, Price, Form, Count, Icon, Detail);
-
+    }
     @PostMapping(value = "deleteMenu")
     public void deleteMenu(@RequestBody String Id) {
         getMenuService.deleteMenu(Id);
